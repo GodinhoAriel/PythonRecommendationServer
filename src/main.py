08ea802322196ecf7f625b0902f3eb8bdd7439e5
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-15 21:20:54 -0200

    Test weather API fixed

--- src/main.py
+++ src/main.py
@@ -5,14 +5,9 @@ app = Flask(__name__)
 def index():
 	return "Hello World"
 
-@app.route('/temperature', methods=['POST'])
+@app.route('/temperature', methods=['GET'])
 def temperature():
-    zipcode = request.form['zip']
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+94040+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
-    json_object = r.json()
-    temp_k = float(json_object['main']['temp'])
-    temp_f = (temp_k - 273.15) * 1.8 + 32
-	return temp_f
+	return "teste"
 
 if __name__=='__main__':
 	# This is used when running locally only. When deploying to Google App
