c003130e2d70c6a5fa69e7a04f7b4182971eda7f
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-15 21:52:55 -0200

    Test Request implemented

--- src/main.py
+++ src/main.py
@@ -1,25 +1,16 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
-db = client.server
 from flask import Flask 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
-	db = client.server
-	user = db.users.find_one({'id' : '12152580425'})
+	# client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
+	# db = client.server
+	# user = db.users.find_one({'id' : '12152580425'})
 	return "done"
 
-@app.route('/test')
-def test():
-	client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
-	db = client.server
-	user = db.users.find_one({'id' : '12152580425'})
-	return user
-	
 @app.route('/example/')
 def example():
     return {'hello': 'world'}
