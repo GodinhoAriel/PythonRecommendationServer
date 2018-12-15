7a3015fe44cf7219eedfed9e5dbb19da34e34451
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-15 21:12:41 -0200

    Test weather API

--- src/main.py
+++ src/main.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 def index():
 	return "Hello World"
 
+@app.route('/temperature', methods=['POST'])
+def temperature():
+    zipcode = request.form['zip']
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    json_object = r.json()
+    temp_k = float(json_object['main']['temp'])
+    temp_f = (temp_k - 273.15) * 1.8 + 32
+return render_template('temperature.html', temp=temp_f)
+
 if __name__=='__main__':
 	# This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
