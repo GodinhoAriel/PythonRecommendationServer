cb302bba17d3fcf0543395c504cfcd72cf5f34ed
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-15 21:36:50 -0200

    Test Request implemented

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import sys
 import pymongo
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
 db = client.server
