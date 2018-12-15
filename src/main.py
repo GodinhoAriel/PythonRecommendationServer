e62f744e17821acc32ca7fafb1dbe2a075be8494
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-15 21:18:15 -0200

    Test weather API fixed

--- src/main.py
+++ src/main.py
@@ -8,11 +8,11 @@ def index():
 @app.route('/temperature', methods=['POST'])
 def temperature():
     zipcode = request.form['zip']
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+94040+',us&appid=fd38d62aa4fe1a03d86eee91fcd69f6e')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
     temp_f = (temp_k - 273.15) * 1.8 + 32
-return render_template('temperature.html', temp=temp_f)
+	return temp_f
 
 if __name__=='__main__':
 	# This is used when running locally only. When deploying to Google App
