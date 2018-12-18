ab2629762a8101ff2b95720d6c28e95105e222b8
Ariel Godinho <godinho.ariel@gmail.com>
2018-12-18 02:54:42 -0200

    Rollback.

--- src/music_clustering.py
+++ src/music_clustering.py
@@ -1,7 +1,7 @@
 import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-#from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans
 import math
 
 client = MongoClient('mongodb://musicclustering:o5oF111QxnPaMXmk@clustermdb-shard-00-00-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-01-gg5i3.gcp.mongodb.net:27017,clustermdb-shard-00-02-gg5i3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=ClusterMDB-shard-0&authSource=admin&retryWrites=true')
